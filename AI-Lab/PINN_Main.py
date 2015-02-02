b27d86eaa7b2decd045ee4961c202cff719ab25a
gabrieloli <gabriel.oli01001@gmail.com>
2015-02-02 16:42:20 -0200

    Create PINN_Main.py

---
+++ AI-Lab/PINN_Main.py
@@ -0,0 +1,12 @@
+#Copyright (C) 2015 Gabriel de Oliveira <gabriel.oli01001@gmail.com>
+
+import os
+import sys
+
+sys.path.append( 'Learn' )
+import Learn_Main as k_learn
+
+enter = raw_input('>>>')
+enter_2 = raw_input('>>>')
+
+k_learn.learn(enter,enter_2)
