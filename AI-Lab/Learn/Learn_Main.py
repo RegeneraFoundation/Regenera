d692092f977904d1733933cb22688112fae00c18
gabrieloli <gabriel.oli01001@gmail.com>
2015-02-02 16:44:29 -0200

    Update Learn_Main.py

--- AI-Lab/Learn/Learn_Main.py
+++ AI-Lab/Learn/Learn_Main.py
@@ -1,13 +1,13 @@
 from __future__ import print_function
 
-import random_in_wordlist.py as choose
+
 
 def learn(x,x_to):
 	blank = ' '
 	to_write_on_file = x + blank + x_to
 	to_write_on_file_inverse = x_to + blank + x
 	if x in open('brain.txt').read():
-		choose.choose('Error','no','maybe')
+		print 'ERROR L1'
 	else:
 		brain = open('brain.txt', 'a')
 		print(to_write_on_file, file=brain)
