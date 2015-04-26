1d25389d6d4e98300b2e8ca3743cc6474d3460f2
gabrieloli <gabriel.oli01001@gmail.com>
2015-04-26 10:02:04 -0300

    Update
    algo que todos gostamos

--- AI-Lab/PINN_Main.py
+++ AI-Lab/PINN_Main.py
@@ -1,4 +1,4 @@
-#Copyright (C) 2015 Gabriel de Oliveira <gabriel.oli01001@gmail.com>
+#!/usr/bin/python
 #[P]seudo
 #[I]ntelligent
 #[N]neural
@@ -11,6 +11,8 @@ import thread
 import threading
 import sys
 import serial
+import speak
+import minimax_tic_tac_toe
 
 serial = serial.Serial('/dev/ttyACM0', 9600)
 serial.write('3')
@@ -18,11 +20,6 @@ serial.write('3')
 os.system("clear")
 
 random = numpy.random.randint(1, 3)
-
-def speak(n):
-    speech = 'espeak -ven-uk-rp -v female3 -k5 -s150' + ' ' + '"' + n + '"' + " 2>/dev/null"
-    print speech
-    os.system(speech)
 
 def raw_input_with_timeout(prompt, timeout=14):    
     timer = threading.Timer(timeout, thread.interrupt_main)
@@ -42,26 +39,42 @@ def raw_input_with_timeout(prompt, timeout=14):
 
 
 if True:
-  serial.write('4')
-	print("[P]seudo")
+  	serial.write('4')
+	print("[P]robably")
 	print("[I]ntelligent")
 	print("[N]neural")
 	print("[N]network")
-	speak("The main core have been started successfully")
-	speak("Beware world, I am Alive")
-	speak("Administrator please, input your command")
+	speak.speak("The main core have been started successfully")
+	speak.speak("Administrator please, input your command")
 	user_input = raw_input_with_timeout("Command ")
 	user_input = user_input.upper()
 	serial.write('5')
 	time.sleep(1)
-
 	if user_input == 'DEBUG':
 	    serial.write('4')
 	    time.sleep(1)
 	    serial.write('5')
 	    time.sleep(0.2)
-	    speak("Developer Mode started")
+	    speak.speak("Developer Mode Started")
+	    os.system('python Development/')#call the development module
+	elif (user_input == 'TIC TAC TOE' or user_input == 'TICTACTOE' or user_input == 'GAME' or user_input == 'PLAY'):
+		os.system('clear')
+		serial.write('4')
+		speak.speak("Let's play Tic Tac Toe")
+		speak.speak("Remember, you are the X, I am the O")
+		speak.speak("Good Luck")
+		if random == 2:
+			speak.speak("or not")
+		os.system('python minimax_tic_tac_toe.py')
+		serial.write('5')
 	elif user_input == 'EXIT':
-	    speak("ok")
+	    speak.speak("Good Bye")
 	elif (user_input == 'RESTART' or user_input == 'RESET'):
-	    speak("done")
+	    speak("Restarting now")
+	elif (user_input == 'HELP'):
+	    speak.speak("module in development, sorry.")
+	    os.system('python Help/')#call the help module
+	elif (user_input == 'SHUTDOWN'):
+	    speak.speak("Shutting down now")
+	    speak.speak("Bye Bye")
+	    os.system("sudo shutdown -h now")
