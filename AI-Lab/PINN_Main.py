c99295f6f36c0ad50e06882859a0906bd8873dd0
gabrieloli <gabriel.oli01001@gmail.com>
2015-05-28 19:31:07 -0300

    Update PINN_Main.py

--- AI-Lab/PINN_Main.py
+++ AI-Lab/PINN_Main.py
@@ -3,78 +3,96 @@
 #[I]ntelligent
 #[N]neural
 #[N]network
-
-import os
-import numpy
-import time
-import thread
-import threading
-import sys
-import serial
-import speak
-import minimax_tic_tac_toe
-
-serial = serial.Serial('/dev/ttyACM0', 9600)
-serial.write('3')
-
-os.system("clear")
-
-random = numpy.random.randint(1, 3)
-
-def raw_input_with_timeout(prompt, timeout=14):    
-    timer = threading.Timer(timeout, thread.interrupt_main)
-    astring = None
-    try:
-        timer.start()
-        astring = raw_input(prompt)
-    except KeyboardInterrupt:
-        pass
-    timer.cancel()
-    for char in astring:
-     time.sleep(0.2)
-     sys.stdout.write(char)
-     sys.stdout.flush()
-
-    return astring
-
-
-if True:
-  	serial.write('4')
-	print("[P]robably")
-	print("[I]ntelligent")
-	print("[N]neural")
-	print("[N]network")
-	speak.speak("The main core have been started successfully")
-	speak.speak("Administrator please, input your command")
-	user_input = raw_input_with_timeout("Command ")
-	user_input = user_input.upper()
-	serial.write('5')
-	time.sleep(1)
-	if user_input == 'DEBUG':
-	    serial.write('4')
-	    time.sleep(1)
-	    serial.write('5')
-	    time.sleep(0.2)
-	    speak.speak("Developer Mode Started")
-	    os.system('python Development/')#call the development module
-	elif (user_input == 'TIC TAC TOE' or user_input == 'TICTACTOE' or user_input == 'GAME' or user_input == 'PLAY'):
-		os.system('clear')
-		serial.write('4')
-		speak.speak("Let's play Tic Tac Toe")
-		speak.speak("Remember, you are the X, I am the O")
-		speak.speak("Good Luck")
-		if random == 2:
-			speak.speak("or not")
-		os.system('python minimax_tic_tac_toe.py')
-		serial.write('5')
-	elif user_input == 'EXIT':
-	    speak.speak("Good Bye")
-	elif (user_input == 'RESTART' or user_input == 'RESET'):
-	    speak("Restarting now")
-	elif (user_input == 'HELP'):
-	    speak.speak("module in development, sorry.")
-	    os.system('python Help/')#call the help module
-	elif (user_input == 'SHUTDOWN'):
-	    speak.speak("Shutting down now")
-	    speak.speak("Bye Bye")
-	    os.system("sudo shutdown -h now")
+import re
+import sqlite3
+from collections import Counter
+from string import punctuation
+from math import sqrt
+ 
+# initialize the connection to the database
+connection = sqlite3.connect('chatbot.sqlite')
+cursor = connection.cursor()
+ 
+# create the tables needed by the program
+try:
+    # create the table containing the words
+    cursor.execute('''
+        CREATE TABLE words (
+            word TEXT UNIQUE
+        )
+    ''')
+    # create the table containing the sentences
+    cursor.execute('''
+        CREATE TABLE sentences (
+            sentence TEXT UNIQUE,
+            used INT NOT NULL DEFAULT 0
+        )''')
+    # create association between weighted words and the next sentence
+    cursor.execute('''
+        CREATE TABLE associations (
+            word_id INT NOT NULL,
+            sentence_id INT NOT NULL,
+            weight REAL NOT NULL)
+    ''')
+except:
+    pass
+ 
+def get_id(entityName, text):
+    """Retrieve an entity's unique ID from the database, given its associated text.
+    If the row is not already present, it is inserted.
+    The entity can either be a sentence or a word."""
+    tableName = entityName + 's'
+    columnName = entityName
+    cursor.execute('SELECT rowid FROM ' + tableName + ' WHERE ' + columnName + ' = ?', (text,))
+    row = cursor.fetchone()
+    if row:
+        return row[0]
+    else:
+        cursor.execute('INSERT INTO ' + tableName + ' (' + columnName + ') VALUES (?)', (text,))
+        return cursor.lastrowid
+ 
+def get_words(text):
+    """Retrieve the words present in a given string of text.
+    The return value is a list of tuples where the first member is a lowercase word,
+    and the second member the number of time it is present in the text."""
+    wordsRegexpString = '(?:\w+|[' + re.escape(punctuation) + ']+)'
+    wordsRegexp = re.compile(wordsRegexpString)
+    wordsList = wordsRegexp.findall(text.lower())
+    return Counter(wordsList).items()
+ 
+ 
+B = 'Hello!'
+while True:
+    # output bot's message
+    print('B: ' + B)
+    # ask for user input; if blank line, exit the loop
+    H = raw_input('H: ').strip()
+    if H == '':
+        break
+    # store the association between the bot's message words and the user's response
+    words = get_words(B)
+    words_length = sum([n * len(word) for word, n in words])
+    sentence_id = get_id('sentence', H)
+    for word, n in words:
+        word_id = get_id('word', word)
+        weight = sqrt(n / float(words_length))
+        cursor.execute('INSERT INTO associations VALUES (?, ?, ?)', (word_id, sentence_id, weight))
+    connection.commit()
+    # retrieve the most likely answer from the database
+    cursor.execute('CREATE TEMPORARY TABLE results(sentence_id INT, sentence TEXT, weight REAL)')
+    words = get_words(H)
+    words_length = sum([n * len(word) for word, n in words])
+    for word, n in words:
+        weight = sqrt(n / float(words_length))
+        cursor.execute('INSERT INTO results SELECT associations.sentence_id, sentences.sentence, ?*associations.weight/(4+sentences.used) FROM words INNER JOIN associations ON associations.word_id=words.rowid INNER JOIN sentences ON sentences.rowid=associations.sentence_id WHERE words.word=?', (weight, word,))
+    # if matches were found, give the best one
+    cursor.execute('SELECT sentence_id, sentence, SUM(weight) AS sum_weight FROM results GROUP BY sentence_id ORDER BY sum_weight DESC LIMIT 1')
+    row = cursor.fetchone()
+    cursor.execute('DROP TABLE results')
+    # otherwise, just randomly pick one of the least used sentences
+    if row is None:
+        cursor.execute('SELECT rowid, sentence FROM sentences WHERE used = (SELECT MIN(used) FROM sentences) ORDER BY RANDOM() LIMIT 1')
+        row = cursor.fetchone()
+    # tell the database the sentence has been used once more, and prepare the sentence
+    B = row[1]
+    cursor.execute('UPDATE sentences SET used=used+1 WHERE rowid=?', (row[0],))
